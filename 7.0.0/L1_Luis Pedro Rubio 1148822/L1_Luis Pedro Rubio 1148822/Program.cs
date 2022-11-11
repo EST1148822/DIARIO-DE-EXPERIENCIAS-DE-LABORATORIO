d5096a624dc7514328e875cc121c2b9bfe0f0737
EST1148822 <111794515+EST1148822@users.noreply.github.com>
2022-11-10 22:09:58 -0600

    Add files via upload

---
+++ 7.0.0/L1_Luis Pedro Rubio 1148822/L1_Luis Pedro Rubio 1148822/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su nombre: ");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola mundo");
+        Console.WriteLine("soy " + Nombre);
+
+        /* Console.WriteLine escribe en diferentes líneas */
+
+        Console.WriteLine("Hola mundo ");
+        Console.WriteLine("soy " + Nombre);
+        Console.ReadKey();
+    }
+}
+      
---
+++ 8.0.0/L5 LPRR 1148822/L5 LPRR 1148822/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroEntero =0;
+
+Console.WriteLine("EJERCICIO 1");
+Console.WriteLine("numero Entero");
+
+numeroEntero =  Convert.ToInt32(Console.ReadLine());
+
+if (numeroEntero >= 1)
+{
+    Console.WriteLine("Numero positivo");
+}
+if (numeroEntero <= -1)
+{
+    Console.WriteLine("Numero negativo");
+}
+if (numeroEntero == 0)
+{ 
+    Console.WriteLine("El numero que ingreso es cero");
+}
