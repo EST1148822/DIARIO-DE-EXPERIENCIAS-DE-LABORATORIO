ff1d5d3a1ae40b65b7bda8d0e3af8b47677ef2f6
EST1148822 <111794515+EST1148822@users.noreply.github.com>
2022-11-10 22:12:10 -0600

    Add files via upload

---
+++ 10.0.0/L10_LPRR_1148822/L10_LPRR_1148822/Program.cs
@@ -0,0 +1,47 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int fallas = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Ingrese su usuario");
+            string Miusuario = Console.ReadLine();
+            Console.WriteLine("Ingrese su contraseña:");
+            string Micontraseña = Console.ReadLine();
+            string res = Login(Miusuario, Micontraseña);
+            
+            if (res == "Verdadero")
+            {
+                Console.WriteLine(res);
+                break;
+            }
+
+            Console.WriteLine(res);
+            fallas++;
+
+        }
+        if (fallas == 3)
+
+        {
+            Console.WriteLine("Limite de intentos alcanzado");
+        }
+           
+        
+    }
+
+    public static string Login(string Miusuario, string Micontraseña)
+
+    { if (Miusuario == "usuario1" && Micontraseña == "asdasd")
+            {
+                return "Verdadero";
+            }
+            else
+
+            {
+                return "Usuario o Contraseña incorrecta.";
+            }
+
+            
+        }
+}
---
+++ 10.0.0/L11_LPRR_1148822/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    public static double conversionMoneda(double dolares)
+    {
+
+        double euros = dolares * 0.75;
+        return euros;
+    }
+    public static double conversionMoneda2(double libra)
+    {
+        double euros2 = libra * 1.22;
+        return euros2;
+    }
+    public static double conversionMoneda3(double yenes)
+    {
+        double euros3 = yenes * 0.009;
+        return euros3;
+    }
+
+    static void Main()
+    {
+        
+        double e = 0, d = 0;
+
+        Console.WriteLine("Ingrese monto en dolares");
+        e = Double.Parse(Console.ReadLine());
+        d = conversionMoneda(e);
+        Console.WriteLine("En euros " + d);
+
+        double a, b;
+
+        Console.WriteLine("Ingrese monto en libras");
+        a = Convert.ToDouble(Console.ReadLine());
+        b = conversionMoneda2(a);
+        Console.WriteLine("En euros " + b);
+
+        double x, y;
+
+        Console.WriteLine("Ingrese monto en yenes");
+        x = Double.Parse(Console.ReadLine());
+        y = conversionMoneda3(x);
+        Console.WriteLine("En euros " + y);
+
+    }
+}
